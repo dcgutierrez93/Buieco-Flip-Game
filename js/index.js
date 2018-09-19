eaa6f41440e57ce8262e0ef868e022c36f2a72ad
dcgutierrez93 <dcgutierrez93@yahoo.com>
2018-09-19 09:56:52 -0500

    Adding Css

---
+++ js/index.js
@@ -0,0 +1,9 @@
+// Cards in array
+let card = document.getElementsByClassName("card");
+let cards = [...card]
+
+// deck of all cards in game
+const deck = document.getElementById("card-deck");
+
+// Var for matchedCards
+let matchedCards = document.getElementsByClassName("match");
