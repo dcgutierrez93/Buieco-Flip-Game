b2fc1590ed708bad620bd64856501fdd9117c1e2
dcgutierrez93 <dcgutierrez93@yahoo.com>
2018-09-21 09:23:24 -0500

    Adding Js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,17 @@
 // Cards in array
 let card = document.getElementsByClassName("card");
-let cards = [...card]
+let cards = [...card];
 
+// loop to add event listeners to each card
+for (var i = 0; i < cards.length; i++) {
+    cards[i].addEventListener("click", displayCard);
+}
+
+var displayCard = function () {
+    this.classList.toggle("open");
+    this.classList.toggle("show");
+    this.classList.toglle("disabled");
+}
 // deck of all cards in game
 const deck = document.getElementById("card-deck");
 
@@ -11,7 +21,16 @@ let matchedCards = document.getElementsByClassName("match");
 // array for opened cards
 var openedCards = [];
 
-// Function Shuffle Deck
+// Function Shuffle Deck - on load and restart
 function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
 
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+    return array;
 }
