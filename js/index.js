81a62ba128ffbbcae6e4dfb994ae96a9cb34cfde
dcgutierrez93 <dcgutierrez93@yahoo.com>
2018-09-21 09:12:12 -0500

    Adding Cards

--- js/index.js
+++ js/index.js
@@ -7,3 +7,11 @@ const deck = document.getElementById("card-deck");
 
 // Var for matchedCards
 let matchedCards = document.getElementsByClassName("match");
+
+// array for opened cards
+var openedCards = [];
+
+// Function Shuffle Deck
+function shuffle(array) {
+
+}
